c40e3097fdddaf9761abaefdf4c87a8995c38458
Laura <120367965+LCAW@users.noreply.github.com>
2023-02-04 11:24:22 +0100

    Create Noten.c

---
+++ Noten.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int moduleingabe(){
+int a,b;
+printf("\n\n Bitte gebe die passende Zahl zum Modul ein\n Management Basics = 1\n Wirtschaftsinformatik Basics = 2\n Mathe = 3\n KdPP = 4\n\n");
+scanf("%d",&a);
+
+if(a=1){
+  return b=10;
+}else if(a=2 || a=3){
+ return b=5;
+  }else{
+   return b=7.5;
+   }
+    
+}
+
+int noteneingabe(b);
+int n;
+printf("\n\n Gib bitte das Ergebnis des Moduls ein:  \n\n");
+scanf("%d",&n);
+
+
+
+int main (){
+
