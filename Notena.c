1ebd96a975af6ae066081d5ada8477873eadb2a7
Laura <120367965+LCAW@users.noreply.github.com>
2023-02-04 11:48:32 +0100

    Create Notena.c

---
+++ Notena.c
@@ -0,0 +1,26 @@
+
+int main(){
+  
+  int i,f,n,c,nt,a,gs,gn;
+  
+  printf("Wie viele Kurse möchtest du angeben?");
+  scanf("%d",&f);
+  
+  
+  for(i==f;i==0;i--){
+      if(i!=0){
+  printf("Gib bitte die Credits an: \n\n");
+  scanf("%d",&c);
+  gs+=c;
+  printf("Gib bitte die Note an: \n\n");
+  scanf("%d",&n);
+  
+  nt=n*c;
+  gn+=nt;
+   i--;
+}}
+      
+  
+  printf("der Schnitt beträgt: %d",gs/gn);
+  return 0;
+}
